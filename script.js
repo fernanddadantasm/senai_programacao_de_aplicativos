4b8137056ba448598063afaea834886295759db7
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-11 14:49:31 -0300

    Revisão

--- script.js
+++
@@ -1,6 +0,0 @@
-function gerarBotao() {
-    const idadeInput = document.getElementById('idadeInput');
-    const idade = parseInt(idadeInput.value);
-
-    const resultadoDiv = document.getElementById('resultado');
-    resultadoDiv.innerHTML = ''; // Limpa o conteúdo anterior
